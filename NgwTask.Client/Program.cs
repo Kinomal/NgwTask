d8fb37e0cef8fa2e3aa86420bb8603949759e9b7
Kino <htpersonal@qq.com>
2021-03-31 20:02:23 +0800

    创建

---
+++ NgwTask.Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NgwTask.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 3061;
+            const string ip = "127.0.0.1";
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(new IPEndPoint(IPAddress.Parse(ip), port));
+            Console.WriteLine("连接服务器成功");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NgwTask.Host/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace NgwTask.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 3061;
+            const string ip = "127.0.0.1";
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Parse(ip), port));
+            socket.Listen(10);
+            Console.WriteLine("启动监听{0}成功", socket.LocalEndPoint);
+            var clientSocket = socket.Accept();
+            Console.WriteLine("客户端{0}成功连接", clientSocket.RemoteEndPoint);
+            while (true)
+            {
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
