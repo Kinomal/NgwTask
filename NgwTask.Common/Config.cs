0d8200dfaad66fad5303611c6a77879ab972b506
Kino <htpersonal@qq.com>
2021-04-02 09:58:46 +0800

    错别字

--- NgwTask.Common/Config.cs
+++ NgwTask.Common/Config.cs
@@ -8,7 +8,7 @@
         public const string Ip = "127.0.0.1";
         //头部字节长度
         public const int HeadByteLength = 1;
-        //数据长读字节长度
+        //数据长度字节长度
         public const int DataLengthByteLength = 4;
         //日志文件目录
         public const string FileName = "/ReceiveData.txt";
